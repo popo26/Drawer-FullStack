40c07cbd91a97a42fa0317cffe8bd1e73639e65d
popo26 <mynameispopo26@gmail.com>
2023-12-06 23:44:58 +1300

    search function fixed

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,78 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useDataContext } from "../context/DataContext";
 
-// export default function Search({ data }) {
 export default function Search() {
   const [searchItem, setSearchItem] = useState("");
   const [searchResult, setSearchResult] = useState([]);
-  // const data = useDataContext();
-  const {drawers, scribbles} = useDataContext();
+  const { drawers, scribbles } = useDataContext();
 
-   //Testing out search results with userId1
-   const searchKeywordInDb = () => {
+  //Testing out search results with userId1
+
+  const searchKeywordInDb = () => {
     let searchResultArray = [];
-    //console.log("Data ", data)
-    let dataValues = Object.values(scribbles);
-    for (let x in dataValues) {
-      //console.log(dataValues[x])
-      for (let y in dataValues[x]) {
-        //console.log(dataValues[x][y])
-        if (
-          dataValues[x][y]["userId"] == 1 &&
-          dataValues[x][y]["type"] == "scribble" &&
-          dataValues[x][y]["title"]
+    for (let x in scribbles) {
+      if (
+        (scribbles[x]["userId"] == 1 &&
+          // scribbles[x]["type"] == "scribble" &&
+          scribbles[x]["title"]
             .toLowerCase()
-            .includes(searchItem.toLowerCase())
-        ) {
-          searchResultArray.push(dataValues[x][y]);
-        }
+            .includes(searchItem.toLowerCase())) ||
+        scribbles[x]["content"].toLowerCase().includes(searchItem.toLowerCase())
+      ) {
+        searchResultArray.push(scribbles[x]);
       }
     }
     return searchResultArray;
   };
 
-  // //Testing out search results with userId1 - ORIGINAL
-  // const searchKeywordInDb = () => {
-  //   let searchResultArray = [];
-  //   //console.log("Data ", data)
-  //   let dataValues = Object.values(data);
-  //   for (let x in dataValues) {
-  //     //console.log(dataValues[x])
-  //     for (let y in dataValues[x]) {
-  //       //console.log(dataValues[x][y])
-  //       if (
-  //         dataValues[x][y]["userId"] === 1 &&
-  //         dataValues[x][y]["type"] == "scribble" &&
-  //         dataValues[x][y]["title"]
-  //           .toLowerCase()
-  //           .includes(searchItem.toLowerCase())
-  //       ) {
-  //         searchResultArray.push(dataValues[x][y]);
-  //       }
-  //     }
-  //   }
-  //   return searchResultArray;
-  // };
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Clicked");
     const result = searchKeywordInDb();
     setSearchResult(result);
-    // return result;
   };
 
   const handleChange = (e) => {
-    console.log(e.target.value);
     setSearchItem(e.target.value);
   };
 
   return (
     <div className="Search">
       <div className="">
-        <form >
+        <form>
           <input
             className="search-input"
             onChange={handleChange}
@@ -81,19 +49,25 @@ export default function Search() {
             value={searchItem}
             placeholder="Enter keyword.."
           />
-          <Button variant="dark" onClick={handleSubmit}>Search</Button>
+          <Button variant="dark" onClick={handleSubmit}>
+            Search
+          </Button>
         </form>
 
         <div>
-          {searchResult.map((item) => {
-            return (
-              <Link to={`/scribble/${item._id}`} key={item._id}>
-                <p>
-                  ID{item._id}: {item.title}
-                </p>
-              </Link>
-            );
-          })}
+          {searchResult.length != 0 ? (
+            searchResult.map((item) => {
+              return (
+                <Link to={`/scribble/${item._id}`} key={item._id}>
+                  <p>
+                    ID{item._id}: {item.title}
+                  </p>
+                </Link>
+              );
+            })
+          ) : (
+            <p>No Result.</p>
+          )}
         </div>
       </div>
     </div>
