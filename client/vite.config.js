bc5997df844f869c32e4cab71f49fc68eb855708
popo26 <mynameispopo26@gmail.com>
2024-01-02 09:02:26 +1300

    1 test setup passed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +7,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'happy-dom',
-    setupFiles: './tests/setup.js',
+    // setupFiles: './tests/setup.js',
+    setupFiles: ["./tests/setup.js"],
   },
-})
+});
