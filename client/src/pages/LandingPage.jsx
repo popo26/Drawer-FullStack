94683d622f1a6c9b8cfea75e30e810850478c3c4
popo26 <mynameispopo26@gmail.com>
2024-01-02 08:09:12 +1300

    vitest libaries and dependencies added

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -7,6 +7,7 @@ export default function LandingPage() {
   const navigate = useNavigate();
   return (
     <div className="LandingPage">
+      <h1>Vite</h1>
       <img src={logo} alt="logo" onClick={()=>navigate('/login')}/>
     </div>
   );
