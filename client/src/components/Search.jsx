1ea80e78b0a117655c318400173f05b03b68669e
popo26 <mynameispopo26@gmail.com>
2024-01-10 23:54:40 +1300

    search function enhanced. now can search drawer as well as scribble

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -9,12 +9,13 @@ import { useUserContext } from "../context/UserContext";
 export default function Search() {
   const [searchItem, setSearchItem] = useState("");
   const [searchResult, setSearchResult] = useState([]);
-  const { scribbles } = useDataContext();
+  const { scribbles, drawers } = useDataContext();
   const { user } = useUserContext();
 
   ///++++++++++++++++++++++Search function++++++++++++++++++++++++++++++++++++++
   const searchKeywordInDb = () => {
     let searchResultArray = [];
+    //Search results in scribbles
     for (let item in scribbles) {
       if (
         (scribbles[item]["userId"] === user._id &&
@@ -29,7 +30,41 @@ export default function Search() {
         searchResultArray.push(scribbles[item]);
       }
     }
+    //Search results in drawers
+    for (let item in drawers) {
+      if (
+        drawers[item]["userId"] === user._id &&
+        drawers[item]["name"].toLowerCase().includes(searchItem.toLowerCase())
+      ) {
+        searchResultArray.push(drawers[item]);
+      }
+    }
     return searchResultArray;
+  };
+
+  // ///++++++++++++++++++++++Create search result list++++++++++++++++++++++++++++++++++++++
+  const createSearchResultList = (resultList) => {
+    let results = [];
+    for (let obj of resultList) {
+      if (obj.type === "scribble") {
+        results.push(
+          <Link to={`/scribble/${obj._id}`} key={obj._id}>
+            <p>
+              {obj.title} <Icon icon="tabler:scribble" color="#EA4C4C" />
+            </p>
+          </Link>
+        );
+      } else if (obj.type === "drawer") {
+        results.push(
+          <Link to={`/drawer-list/${obj.rootId}`} key={obj._id}>
+            <p>
+              {obj.name} <Icon icon="mingcute:drawer-line" color="#EA4C4C" />
+            </p>
+          </Link>
+        );
+      }
+    }
+    return results;
   };
 
   //++++++++++++++++++++++++++Get the result and set it as state+++++++++++++++++
@@ -62,14 +97,8 @@ export default function Search() {
         </form>
 
         <div className="search-result-div">
-          {searchResult.length != 0 ? (
-            searchResult.map((item) => {
-              return (
-                <Link to={`/scribble/${item._id}`} key={item._id}>
-                  <p>{item.title}</p>
-                </Link>
-              );
-            })
+          {searchResult.length !== 0 ? (
+            createSearchResultList(searchResult)
           ) : (
             <p>No Result.</p>
           )}
